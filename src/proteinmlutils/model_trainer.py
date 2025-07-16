ccbe8656cc01faff243e69a700e8799749aba2f6
Mohit Pandey <mpandey@scribetx.com>
2025-07-16 10:47:04 -0700

    Update version to 0.0.22

--- src/proteinmlutils/model_trainer.py
+++ src/proteinmlutils/model_trainer.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import numpy as np
 
+from scipy import stats
 import sklearn as sk
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
-
+import xgboost as xgb
 from sklearn.model_selection import KFold, GroupKFold
 
 class classifier_model_trainer:
     '''
-    It trains a classifier model using RandomForestClassifier. It can take a list of linear sequences and split the data into training and testing sets. It can also perform k-group cross-validation. The function is used in the RandomForest_classifier_trainer method.
+    It trains a classifier model. It can take a list of linear sequences and split the data into training and testing sets. It can also perform k-group cross-validation. The function is used in the RandomForest_classifier_trainer method.
     Input:
     -----------------
     X_train: a 2D array of size (N_features, N_samples)
@@ -64,9 +65,22 @@ class classifier_model_trainer:
                 raise ValueError("Strategy must be either 'random' or 'groupkfold'")
         
 
+
+    def kfold_trainer_using_random_grid_search(self, classifier_model, param_grid, n_jobs=1):
+        
+        random_search = sk.model_selection.RandomizedSearchCV(classifier_model,param_grid, cv=self.cv_splitter, n_iter=self.n_iter, scoring=self.scoring, refit=self.primary_scoring, n_jobs=n_jobs, verbose=2, return_train_score=False)
+        random_search.fit(self.X_train,self.y_train)
+        random_search_results_df=pd.DataFrame(random_search.cv_results_)
+        best_model_found=random_search.best_estimator_
+        y_pred_train=best_model_found.predict(self.X_train)
+        print("Best parameters found: ", random_search.best_params_)
+        print("Score on training: Recall=", sk.metrics.recall_score(self.y_train,y_pred_train), "Precision=", sk.metrics.precision_score(self.y_train,y_pred_train), "F1 score=",sk.metrics.f1_score(self.y_train, y_pred_train))
+        
+        return best_model_found, random_search_results_df
+
     def RandomForestClassifier_trainer(self, training_mode='default', **kwargs):
         '''
-        Returns a Random Forest Classifier model with default parameters.
+        Returns a Random Forest Classifier model with default parameters. The model can be trained for multi-class or binary classification.
         '''
         
         if kwargs.get('param_grid') is not None:
@@ -97,13 +111,7 @@ class classifier_model_trainer:
             else:
                 n_jobs = -1
 
-            random_search = sk.model_selection.RandomizedSearchCV(classifier_model,param_grid, cv=self.cv_splitter, n_iter=self.n_iter, scoring=self.scoring, refit=self.primary_scoring, n_jobs=n_jobs, verbose=2, return_train_score=False)
-            random_search.fit(self.X_train,self.y_train)
-            random_search_results_df=pd.DataFrame(random_search.cv_results_)
-            best_model_found=random_search.best_estimator_
-            y_pred_train=best_model_found.predict(self.X_train)
-            print("Best parameters found: ", random_search.best_params_)
-            print("Score on training: Recall=", sk.metrics.recall_score(self.y_train,y_pred_train), "Precision=", sk.metrics.precision_score(self.y_train,y_pred_train), "F1 score=",sk.metrics.f1_score(self.y_train, y_pred_train))
+            best_model_found, random_search_results_df=self.kfold_trainer_using_random_grid_search(classifier_model, param_grid, n_jobs=1)
             return best_model_found, random_search_results_df
 
 
@@ -111,3 +119,71 @@ class classifier_model_trainer:
             raise ValueError("Training mode must be either 'default' or 'CrossVal'")
         
 
+    def XGBoost_classifier_trainer(self, N_categories, training_mode='default', **kwargs):
+        #TODO: Add docstrings. Add purpose of the function. Add documentation for the variables and datatypes. Add comment on the fact the XGBoost is preferred.
+
+        '''
+        The purpose it to train a XGBoost classifier
+        Input:
+        N_categories: an integer. The number of categories for classification.
+        kfold: an integer. The number of folds for cross-validation.
+        **kwargs: a dictionary. It can have 'n_iter' key. It is used to provide the number of iterations for RandomizedSearchCV.
+        Output:
+        best_model_found: a trained XGBoost model
+        grid_search_results_df: a pandas dataframe that has the results of RandomizedSearchCV
+        data_training: a tuple of X_train, y_train
+        data_test: a tuple of X_test, y_test
+        linear_seq_data: a tuple of linear_seq_train, linear_seq_test
+        activity_data: a tuple of activity_train, activity_test
+        '''
+       
+        classifier_model = xgb.XGBClassifier()
+
+        #bifurcating binary and multi-class classification.
+        
+        if kwargs.get('param_grid') is not None:
+            param_grid = kwargs['param_grid']
+        else:
+            if N_categories>2:
+                param_grid = {'objective':['multi:softmax'],
+                        'learning_rate': stats.uniform(0.01,0.1),
+                        'max_depth': [3,4,5],
+                        'min_child_weight': [1,3,5],
+                        'gamma': stats.uniform(0.0,0.6),
+                        'subsample':  stats.uniform(0.3,0.6),
+                        'colsample_bytree': stats.uniform(0.5, 0.4),
+                        'n_estimators': [50, 100, 250, 500], 'n_jobs':[-1]}
+    
+            elif N_categories==2:
+                    param_grid = {'objective':['binary:logistic'], 
+                        'learning_rate': stats.uniform(0.01,0.1),
+                    'max_depth': [1,2,5,10,20],
+                    'min_child_weight': [1,3,5],
+                    'gamma': stats.uniform(0.0,0.6),
+                    'subsample':  stats.uniform(0.3,0.6),
+                    'colsample_bytree': stats.uniform(0.5, 0.4),
+                    'n_estimators': [30,50,100,500,1000], 'n_jobs':[-1]} 
+    
+            
+        if training_mode == 'default':
+            classifier_model.fit(self.X_train, self.y_train)
+            y_pred_train = classifier_model.predict(self.X_train)
+            
+            print("Score on training: Recall=", sk.metrics.recall_score(self.y_train,y_pred_train), "Precision=", sk.metrics.precision_score(self.y_train,y_pred_train), "F1 score=",sk.metrics.f1_score(self.y_train, y_pred_train))
+        
+    
+            return classifier_model, pd.DataFrame({'y_true': self.y_train, 'y_pred': y_pred_train})
+
+        elif training_mode == 'CrossVal':
+        
+            if kwargs.get('n_jobs') is not None:
+                assert n_jobs ==1, "n_jobs should be 1 for XGBoost CrossVal training mode"
+            else:
+                n_jobs = 1
+
+
+            best_model_found, random_search_results_df=self.kfold_trainer_using_random_grid_search(classifier_model, param_grid, n_jobs=1)
+            return best_model_found, random_search_results_df
+        else:
+            raise ValueError("Training mode must be either 'default' or 'CrossVal'")
+        
