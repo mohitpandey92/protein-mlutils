2ac21f78ae6d651db99d8c656a5d1abd9da64d0a
Mohit Pandey <mpandey@scribetx.com>
2025-07-16 21:36:33 -0700

    Update version to 0.0.26

--- src/proteinmlutils/model_trainer.py
+++ src/proteinmlutils/model_trainer.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-
 from scipy import stats
 import sklearn as sk
 
@@ -11,13 +10,11 @@ from sklearn.model_selection import KFold, GroupKFold
 
 class classifier_model_trainer:
     '''
-    It trains a classifier model. It can take a list of linear sequences and split the data into training and testing sets. It can also perform k-group cross-validation. The function is used in the RandomForest_classifier_trainer method.
+    It trains a classifier model with the given X_train and y_train.  It can perform k-group cross-validation. The function is used in the RandomForest_classifier_trainer method.
     Input:
     -----------------
     X_train: a 2D array of size (N_features, N_samples)
     y_train: a 1D of size N_samples
-    X_test: a 2D array of size (N_features, N_samples)
-    y_test: a 1D of size N_samples
     strategy: a string. Options are 'random', 'groupkfold'
     kfold: an integer. The number of folds for cross-validation.
     **kwargs: a dictionary. It can have 'cv_splitter' and 'param_dict' keys. It is used to provide the cv_splitter and parameters for the model.
@@ -26,8 +23,6 @@ class classifier_model_trainer:
     ----------------
     best_model_found: a trained RandomForest model
     random_search_results_df: a pandas dataframe that has the results of RandomizedSearchCV
-    (X_train, y_train): tuple of training data
-    (X_test, y_test): tuple of test data
     '''
     def __init__(self, X_train, y_train, strategy='random', kfold=3, **kwargs):
     
@@ -61,6 +56,11 @@ class classifier_model_trainer:
                 self.cv_splitter = sk.model_selection.KFold(n_splits=self.kfold, shuffle=True, random_state=42)
             elif self.strategy == 'groupkfold':
                 self.cv_splitter = sk.model_selection.GroupKFold(n_splits=self.kfold)
+                raise ValueError("GroupKFold is not supported for classification tasks. Use 'random' strategy instead.")
+                #self.groups = kwargs.get("groups")
+                #if self.groups is None:
+                #    raise ValueError("Groups must be provided for GroupKFold strategy.")
+                
             else:
                 raise ValueError("Strategy must be either 'random' or 'groupkfold'")
         
@@ -165,20 +165,22 @@ class classifier_model_trainer:
                     'n_estimators': [30,50,70,100], 'n_jobs':[-1]} 
 
             else:
-                raise ValueError("N_categories must be either 2 or greater than 2 for binary and multi-class classification respectively.")        
+                raise ValueError("N_categories must be either 2 or greater than 2 for binary and multi-class classification respectively.")
+            
         if training_mode == 'default':
             classifier_model.fit(self.X_train, self.y_train)
             y_pred_train = classifier_model.predict(self.X_train)
             
             print("Score on training: Recall=", sk.metrics.recall_score(self.y_train,y_pred_train), "Precision=", sk.metrics.precision_score(self.y_train,y_pred_train), "F1 score=",sk.metrics.f1_score(self.y_train, y_pred_train))
         
-    
+
             return classifier_model, pd.DataFrame({'y_true': self.y_train, 'y_pred': y_pred_train})
 
         elif training_mode == 'CrossVal':
         
             if kwargs.get('n_jobs') is not None:
-                assert n_jobs ==1, "n_jobs should be 1 for XGBoost CrossVal training mode"
+                n_jobs = kwargs['n_jobs']
+                assert n_jobs == 1, "n_jobs should be 1 for XGBoost CrossVal training mode"
             else:
                 n_jobs = 1
 
@@ -188,3 +190,123 @@ class classifier_model_trainer:
         else:
             raise ValueError("Training mode must be either 'default' or 'CrossVal'")
         
+
+
+
+class regressor_model_trainer:
+    '''
+    It trains a regressor model. It can take a list of linear sequences and split the data into training and testing sets. It can also perform k-group cross-validation. The function is used in the RandomForest_regressor_trainer method.
+    Input:
+    -----------------
+    X_train: a 2D array of size (N_features, N_samples)
+    y_train: a 1D of size N_samples
+    X_test: a 2D array of size (N_features, N_samples)
+    y_test: a 1D of size N_samples
+    strategy: a string. Options are 'random', 'groupkfold'
+    kfold: an integer. The number of folds for cross-validation.
+    **kwargs: a dictionary. It can have 'cv_splitter' and 'param_dict' keys. It is used to provide the cv_splitter and parameters for the model.
+
+    Output:
+    ----------------
+    best_model_found: a trained RandomForest model
+    random_search_results_df: a pandas dataframe that has the results of RandomizedSearchCV
+    
+    '''
+    
+    def __init__(self, X_train, y_train, strategy='random', kfold=3, **kwargs):
+
+        self.X_train = X_train
+        self.y_train = y_train
+        self.kfold = kfold
+        self.strategy = strategy
+
+        if kwargs.get("scoring") is not None:
+            self.scoring = kwargs["scoring"]
+        else:
+            self.scoring = ["neg_mean_squared_error", "r2", "neg_mean_absolute_error"]
+
+        if kwargs.get("n_iter") is not None:
+            self.n_iter = kwargs["n_iter"]
+        else:
+            self.n_iter = 2
+            
+        if kwargs.get("primary_scoring") is not None:
+            self.primary_scoring = kwargs["primary_scoring"]
+        else:
+            self.primary_scoring = 'neg_mean_absolute_error'
+            
+        if kwargs.get('cv_splitter') is not None:
+            self.cv_splitter = kwargs['cv_splitter']
+        else:
+            if self.strategy == 'random':
+                self.cv_splitter = sk.model_selection.KFold(n_splits=self.kfold, shuffle=True, random_state=42)
+            elif self.strategy == 'groupkfold':
+                raise ValueError("GroupKFold is not supported for regression tasks. Use 'random' strategy instead.")
+                #self.cv_splitter = sk.model_selection.GroupKFold(n_splits=self.kfold)
+                #self.groups = kwargs.get("groups")
+            else:
+                raise ValueError("Strategy must be either 'random' or 'groupkfold'")
+
+    
+    
+    def kfold_trainer_using_random_grid_search(self, classifier_model, param_grid, n_jobs=1):
+        
+        random_search = sk.model_selection.RandomizedSearchCV(classifier_model,param_grid, cv=self.cv_splitter, n_iter=self.n_iter, scoring=self.scoring, refit=self.primary_scoring, n_jobs=n_jobs, verbose=2, return_train_score=False)
+        random_search.fit(self.X_train,self.y_train)
+        random_search_results_df=pd.DataFrame(random_search.cv_results_)
+        best_model_found=random_search.best_estimator_
+        y_pred_train=best_model_found.predict(self.X_train)
+        print("Best parameters found: ", random_search.best_params_)
+        print("Score on training: MAE=", sk.metrics.mean_absolute_error(self.y_train,y_pred_train), "R2=", sk.metrics.r2_score(self.y_train,y_pred_train))
+
+        return best_model_found, random_search_results_df
+    
+    def XGBoostRegressor_trainer(self, training_mode='default', **kwargs):
+        
+        '''
+        The purpose it to train a XGBoost regressor
+        Input:
+        **kwargs: a dictionary. It can have 'n_iter' key. It is used to provide the number of iterations for 
+        param_grid: a dictionary. It is used to provide the parameters for the model.
+        training_mode: a string. Options are 'default', 'CrossVal'. If 'default', it will train the model with default parameters. If 'CrossVal', it will perform cross-validation using RandomizedSearchCV.
+        Output:
+        best_model_found: a trained XGBoost model
+        grid_search_results_df: a pandas dataframe that has the results of RandomizedSearchCV
+        '''
+
+        regressor_model = xgb.XGBRegressor()
+
+        if kwargs.get('param_grid') is not None:
+            param_grid = kwargs['param_grid']
+        else:
+            param_grid = {'objective':['reg:absoluteerror'],
+                        'learning_rate': stats.uniform(0.01,0.1),
+                        'max_depth': [3,4,5,6,7,8,9,10], #3 is default
+                        'min_child_weight': [1,3,5],
+                        'gamma': stats.uniform(0.0,0.6),
+                        'subsample':  stats.uniform(0.3,0.6),
+                        'colsample_bytree': stats.uniform(0.5, 0.4),
+                        'n_estimators': [30,50,100, 200,300, 500], 'n_jobs':[-1]}
+        
+        if training_mode == 'default':
+            regressor_model.fit(self.X_train, self.y_train)
+            y_pred_train = regressor_model.predict(self.X_train)
+
+            print("Score on training: MAE=", sk.metrics.mean_absolute_error(self.y_train,y_pred_train), "R2=", sk.metrics.r2_score(self.y_train,y_pred_train))
+
+            return regressor_model, pd.DataFrame({'y_true': self.y_train, 'y_pred': y_pred_train})
+
+        elif training_mode == 'CrossVal':
+        
+            if kwargs.get('n_jobs') is not None:
+                n_jobs = kwargs['n_jobs']
+                assert n_jobs ==1, "n_jobs should be 1 for XGBoost CrossVal training mode"
+            else:
+                n_jobs = 1
+
+
+            best_model_found, random_search_results_df=self.kfold_trainer_using_random_grid_search(regressor_model, param_grid, n_jobs=n_jobs)
+            return best_model_found, random_search_results_df
+        else:
+            raise ValueError("Training mode must be either 'default' or 'CrossVal'")
+    
