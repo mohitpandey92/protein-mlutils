5192e0de516471f725b56ff73ca074c43f558c54
mpandey <mpandey@scribetx.com>
2025-10-07 22:34:16 +0000

    Update version to 0.0.56. removed hyperparam log

--- update_version.sh
+++ update_version.sh
@@ -1,11 +1,12 @@
 #update the version in pyproject.toml
 # and commit the changes
 # usage: ./update_package.sh <new_version>
-if [ -z "$1" ]; then
-  echo "Usage: $0 <new_version>"
+if [ -z "$2" ]; then
+  echo "Usage: $0 <new_version> <commit_message>"
   exit 1
 fi  
 NEW_VERSION=$1
+commit_message=$2
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sed -i "s/version = .*/version = \"$NEW_VERSION\"/" pyproject.toml
@@ -19,6 +20,6 @@ echo "Recognized $OSTYPE. Updated version to $NEW_VERSION in pyproject.toml"
 rm -rf dist
 python -m build &&  python -m pip install --upgrade dist/*.gz
 git add pyproject.toml src/proteinmlutils/*.py README.md notebook/*.ipynb *.sh
-git commit -m "Update version to $NEW_VERSION"
+git commit -m "Update version to $NEW_VERSION. $commit_message"
 git push 
 echo "Version updated to $NEW_VERSION, package built and changes committed."
